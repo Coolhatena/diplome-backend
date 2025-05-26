ac23e8085dc86a1a38dbf6ef2914001f0bc3f002
Coolhatena <ingsc.geffroy@gmail.com>
2025-05-25 22:39:32 -0700

    Create video upload route

--- routes/video.js
+++ routes/video.js
@@ -4,6 +4,20 @@ import Validate from '../middleware/validate.js';
 
 const router = express.Router();
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+	cb(null, 'videos/');
+  },
+  filename: (req, file, cb) => {
+	const uniqueName = `${Date.now()}-${file.originalname}`;
+	cb(null, uniqueName);
+  }
+});
+
+const upload = multer({ storage });
+
 router.get('/video/:filename', SendVideo);
+
+router.post('/video/', upload.single('video'), UploadVideo);
 
 export default router;
