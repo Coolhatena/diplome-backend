a4666d6c15f330c2e554a7617bec0980f9a50a36
Coolhatena <ingsc.geffroy@gmail.com>
2025-05-26 16:29:06 -0700

    FIX: add file extension to import

--- controllers/video.js
+++ controllers/video.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import Video from '../models/Video';
+import Video from '../models/Video.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
