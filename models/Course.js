14be61a39a21386274ff7702d681c204c94573b8
Coolhatena <ingsc.geffroy@gmail.com>
2025-05-27 20:34:55 -0700

    Create model for courses

---
+++ models/Course.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const courseSchema = new mongoose.Schema({
+	title: 
+	{ 
+		type: String, 
+		required: true 
+	},
+	description: 
+	{ 
+		type: String, 
+		required: true 
+	},
+	uploadedAt: 
+	{ 
+		type: Date, 
+		default: Date.now 
+	}
+});
+
+export default mongoose.model('Course', courseSchema);
