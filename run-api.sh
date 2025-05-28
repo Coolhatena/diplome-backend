90261666bfd562b631a75984a81d4205652304f7
Coolhatena <ingsc.geffroy@gmail.com>
2025-05-28 15:31:13 -0700

    Use run dev to start the project

--- run-api.sh
+++ run-api.sh
@@ -1,2 +1,2 @@
 xfce4-terminal -T "Mongo Server" -e "bash -ic 'mongoserver'" # "mongoserver" is an alias for "mongod --dbpath=/path/to/dbs"
-xfce4-terminal -T "API" -e "npm start"
+xfce4-terminal -T "API" -e "npm run dev"
